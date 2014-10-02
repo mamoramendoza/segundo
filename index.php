fd5ea107c74750d29626c11fcbbddbf547d00a10
Miguel Angel Mora Mendoza <mamoramendoza@gmail.com>
2014-10-02 14:31:11 +0200

    Añadido formulario de entrada

--- index.php
+++ index.php
@@ -5,5 +5,13 @@
 	<meta charset="utf-8"/>
   </head>
   <body lang="es">
+  	 <form method="post">
+         <fieldset>
+           <legend>Fomulario de entrada</legend>
+           Usuario: <input type="text" name="usuario"><br/>
+           Contraseña: <input type="password" name="contraseña"><br/><br/>
+           <button type="submit" valeu="Enviar">Enviar</button>
+         </fieldset>
+  	 </form>
   </body>
 </html>
